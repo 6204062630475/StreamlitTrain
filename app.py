941905dacc9519730b2ad026a301e3b09862e1a4
Chayakorn Satpan <74363236+6204062630475@users.noreply.github.com>
2023-04-23 07:35:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,44 +1,17 @@
+import cv2
 import streamlit as st
-from PIL import Image
-import torch
-import numpy as np
 
-from streamlit_webrtc import webrtc_streamer, WebRtcMode, RTCConfiguration
-import av
+def main():
+    st.title("Webcam Live Feed")
+    run = st.checkbox('Run')
+    FRAME_WINDOW = st.image([])
+    camera = cv2.VideoCapture(0)
+    while run:
+        _, frame = camera.read()
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        FRAME_WINDOW.image(frame)
+    else:
+        st.write('Stopped')
 
-
-device = 'cpu'
-if not hasattr(st, 'classifier'):
-    st.model = torch.hub.load('ultralytics/yolov5', 'yolov5s',  _verbose=False)
-    # st.model = torch.hub.load('ultralytics/yolov5', 'custom', path='yolov5s.pt', _verbose=False)
-    
-
-
-RTC_CONFIGURATION = RTCConfiguration(
-    {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
-)
-
-
-class VideoProcessor:
-    def recv(self, frame):
-        img = frame.to_ndarray(format="bgr24")
-        
-        # vision processing
-        flipped = img[:, ::-1, :]
-
-        # model processing
-        im_pil = Image.fromarray(flipped)
-        results = st.model(im_pil, size=112)
-        bbox_img = np.array(results.render()[0])
-
-        return av.VideoFrame.from_ndarray(bbox_img, format="bgr24")
-
-
-webrtc_ctx = webrtc_streamer(
-    key="WYH",
-    mode=WebRtcMode.SENDRECV,
-    rtc_configuration=RTC_CONFIGURATION,
-    video_processor_factory=VideoProcessor,
-    media_stream_constraints={"video": True, "audio": False},
-    async_processing=False,
-)
+if __name__ == '__main__':
+    main()
