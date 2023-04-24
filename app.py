e953828bc534a31a0d5984f0bc11a619216af5e8
Chayakorn Satpan <chayakorn1106@gmail.com>
2023-04-24 14:20:53 +0700

    fishmodel

--- app.py
+++ app.py
@@ -2,14 +2,14 @@ import streamlit as st
 from PIL import Image
 import torch
 import numpy as np
-
+import cv2
 from streamlit_webrtc import webrtc_streamer, WebRtcMode, RTCConfiguration
 import av
 
 
 device = 'cpu'
 if not hasattr(st, 'classifier'):
-    st.model = torch.hub.load('ultralytics/yolov5', 'yolov5s',  _verbose=False)
+    st.model = torch.hub.load('ultralytics/yolov5', 'custom',path='fishtrainIV0.89.pt',force_reload=True)
     # st.model = torch.hub.load('ultralytics/yolov5', 'custom', path='yolov5s.pt', _verbose=False)
     
 
@@ -29,6 +29,8 @@ class VideoProcessor:
         # model processing
         im_pil = Image.fromarray(flipped)
         results = st.model(im_pil, size=112)
+
+
         bbox_img = np.array(results.render()[0])
 
         return av.VideoFrame.from_ndarray(bbox_img, format="bgr24")
